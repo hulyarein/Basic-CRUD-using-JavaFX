32b4197a2dab296b3ba9c284065ddde44746890f
hulyarein <julialaine.segundo@cit.edu>
2024-04-24 22:23:32 +0800

    Updated version

---
+++ src/main/java/com/example/crud/MakeNoteController.java
@@ -0,0 +1,33 @@
+package com.example.crud;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+
+public class MakeNoteController {
+
+    @FXML
+    private Button btnBack;
+
+    @FXML
+    private Button btnMakeNote;
+
+    @FXML
+    private TextArea txtContent;
+
+    @FXML
+    private TextField txtTitle;
+
+    @FXML
+    void Back(ActionEvent event) {
+
+    }
+
+    @FXML
+    void MakeNote(ActionEvent event) {
+
+    }
+
+}
--- src/main/java/com/example/crud/Note.java
+++ src/main/java/com/example/crud/Note.java
@@ -1,52 +1,71 @@
 package com.example.crud;
 
+import javafx.beans.property.IntegerProperty;
+import javafx.beans.property.StringProperty;
+import javafx.beans.property.SimpleIntegerProperty;
+import javafx.beans.property.SimpleStringProperty;
+
 import java.util.Date;
 
 public class Note {
 
-    private static int idGenerator;
-    private final int id;
-    private String note;
-    private final String username;
-    private String title;
-    private final Date writtenDate;
+    private final IntegerProperty id;
+    private final StringProperty note;
+    private final StringProperty title;
+    private final StringProperty writtenDate;
 
-    public Note(int id, String note, String username, String title, Date writtenDate) {
-        this.id = id;
-        this.note = note;
-        this.username = username;
-        this.writtenDate = writtenDate;
-        this.title = title;
+    public Note(Integer id, String note,String title, String writtenDate) {
+        this.id = new SimpleIntegerProperty(id);
+        this.note = new SimpleStringProperty(note);
+        this.writtenDate = new SimpleStringProperty(writtenDate);
+        this.title = new SimpleStringProperty(title);
     }
 
-    public static void setIdGenerator(int idGenerator) {
-        Note.idGenerator = idGenerator;
+    public int getId() {
+        return id.get();
+    }
+
+    public IntegerProperty idProperty() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id.set(id);
     }
 
     public String getNote() {
+        return note.get();
+    }
+
+    public StringProperty noteProperty() {
         return note;
     }
 
     public void setNote(String note) {
-        this.note = note;
+        this.note.set(note);
     }
+
     public String getTitle() {
+        return title.get();
+    }
+
+    public StringProperty titleProperty() {
         return title;
     }
 
-    public void setTitle(String note) {
-        this.title = note;
+    public void setTitle(String title) {
+        this.title.set(title);
     }
 
-    public String getUsername() {
-        return username;
+    public String getWrittenDate() {
+        return writtenDate.get();
     }
 
-    public Date getWriteDate() {
+    public StringProperty writtenDateProperty() {
         return writtenDate;
     }
 
-    public int getId() {
-        return id;
+    public void setWrittenDate(String writtenDate) {
+        this.writtenDate.set(writtenDate);
     }
 }
