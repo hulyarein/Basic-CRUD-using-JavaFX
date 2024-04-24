01722e16674d35731e48856ccbab2224dc923941
hulyarein <julialaine.segundo@cit.edu>
2024-04-24 21:19:42 +0800

    try

--- src/main/java/com/example/crud/MySQLConnection.java
+++ src/main/java/com/example/crud/MySQLConnection.java
@@ -4,6 +4,10 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class MySQLConnection {
     public static final String URL = "jdbc:mysql://localhost:3306/dbsegundo";
     public static final String USER = "root";
@@ -14,20 +18,9 @@ public class MySQLConnection {
             Class.forName("com.mysql.cj.jdbc.Driver");
             c = DriverManager.getConnection(URL,USER,PASS);
             System.out.println("DB Connected");
-        } catch (ClassNotFoundException  | SQLException e) {
+        } catch (ClassNotFoundException | SQLException e) {
             e.printStackTrace();
         }
         return c;
-    }
-
-    public static void main(String[] args) {
-        Connection c = MySQLConnection.getConnection();
-
-        try {
-            c.close();
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-
     }
 }
---
+++ src/main/java/com/example/crud/NotesController.java
@@ -0,0 +1,4 @@
+package com.example.crud;
+
+public class NotesController {
+}
